2286231d3ba7749121684bf30dd52dfdab5f90b8
josrojrom1 <josrojrom1@alum.us.es>
2024-04-30 01:02:12 +0200

    Added colors to identify hosts and gateway

--- netscanner.py
+++ netscanner.py
@@ -25,14 +25,14 @@ def scan_ips(ip_range):
 
 if __name__ == "__main__":
     # IP range pattern
-    ip_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
-
+    ip_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+    ip_gateway = re.compile("^(?:[0-9]{1,3}\.){3}[1]*$")
     
     while True:
         # IP range
         ip_range = input("Insert IP range or private network (Example: 192.168.1.0/24): ")
         print("\n")
-        if ip_pattern.search(ip_range.strip()):
+        if ip_range_pattern.search(ip_range.strip()):
             print(f"   \033[92m> {ip_range} is a valid IP range! <\033[0m")
             break
         else:
@@ -45,6 +45,10 @@ if __name__ == "__main__":
     print("Dispositivos en la red:")
     print('---------------------------------------------')
     for device in devices:
-        print(f"(*) IP: {device['ip']} - MAC: {device['mac']}")
-        print('---------------------------------------------')
+        if ip_gateway.search(device['ip']):
+            print(f"(*) IP: {device['ip']} - MAC: {device['mac']}  \033[38;5;208m> Gateway <\033[0m")
+            print('---------------------------------------------')
+        else:
+            print(f"(*) IP: {device['ip']} - MAC: {device['mac']}  \033[92m> Host <\033[0m")
+            print('---------------------------------------------')
     print("\n")
